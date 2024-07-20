116ee89fe1b678503420b7a3f03e467cab0fedd0
Reyhaneh <reyhanehkhoshghadam@gmail.com>
2024-07-20 20:49:27 +0330

    optimized
    Check Correction While Typing is late for one keyPress!!

--- session 3/typing-app/src/App.tsx
+++ session 3/typing-app/src/App.tsx
@@ -4,6 +4,7 @@ import {
   createContext,
   useContext,
   KeyboardEvent,
+  useCallback,
 } from "react";
 import "./App.css";
 
@@ -92,13 +93,13 @@ function App() {
   const countDown = () => {
     setRemaining((remaining) => remaining - 1);
   };
-  const shuffle = (array: string[]) => {
+  const shuffle = useCallback((array: string[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-  };
+  }, []);
   const [totalWords, setTotalWords] = useState(0);
   const [correctWords, setCorrectWords] = useState(0);
 
@@ -136,19 +137,38 @@ function App() {
       );
     }
   };
+  const checkCorrectionWhileTyping = (typedWord: string) => {
+    if (typedWord !== words[state.index].text.slice(0, typedWord.length)) {
+      setWords((words) => {
+        const newWords = JSON.parse(JSON.stringify(words));
+        newWords[state.index].color = "#F88379";
+        return newWords;
+      });
+    } else {
+      setWords((words) => {
+        const newWords = JSON.parse(JSON.stringify(words));
+
+        newWords[state.index].color = "lightgray";
+        return newWords;
+      });
+    }
+  };
   const keyDown = (event: KeyboardEvent) => {
-    setStartedTyping(true);
     if (event.code === "Space") {
       if (inputText.trim().length >= 1) {
         checkCorrection(inputText.trim());
       }
       setInputText("");
+    } else if (/^[A-Za-z]/.test(event.key)) {
+      checkCorrectionWhileTyping(inputText.trim());
     }
+    setStartedTyping(true);
   };
 
   const updateWords = (color: CurrentWord["color"]) => {
     setWords((words) => {
-      const newWords = structuredClone(words);
+      const newWords = JSON.parse(JSON.stringify(words));
+
       newWords[state.index].color = color;
       if (state.index + 1 < words.length) {
         newWords[state.index + 1].color = "lightgray";
@@ -182,7 +202,12 @@ function App() {
               }}
             />
             <Timer />
-            <button className="retryButton">Reset</button>
+            <button
+              className="retryButton"
+              onClick={() => window.location.reload()}
+            >
+              Reset
+            </button>
           </section>{" "}
         </div>
         <Result />
@@ -193,36 +218,39 @@ function App() {
 
 function TextDisplay() {
   const { remaining } = useContext(TimeContext);
-  const word2D = (words: CurrentWord[], maxChar = 70): CurrentWord[][] => {
-    const setWordLines = (
-      prev: {
-        arr: CurrentWord[];
-        char: number;
-      }[],
-      cur: CurrentWord,
-      index: number,
-      array: CurrentWord[]
-    ) => {
-      if (prev.at(-1)!.char + cur.text.length < maxChar) {
-        prev.at(-1)!.arr.push(cur);
-        prev.at(-1)!.char += cur.text.length;
+  const word2D = useCallback(
+    (words: CurrentWord[], maxChar = 70): CurrentWord[][] => {
+      const setWordLines = (
+        prev: {
+          arr: CurrentWord[];
+          char: number;
+        }[],
+        cur: CurrentWord,
+        index: number,
+        array: CurrentWord[]
+      ) => {
+        if (prev.at(-1)!.char + cur.text.length < maxChar) {
+          prev.at(-1)!.arr.push(cur);
+          prev.at(-1)!.char += cur.text.length;
+          return prev;
+        }
+        prev.push({
+          arr: [cur],
+          char: cur.text.length,
+        });
         return prev;
-      }
-      prev.push({
-        arr: [cur],
-        char: cur.text.length,
-      });
-      return prev;
-    };
-    return words
-      .reduce(setWordLines, [
-        {
-          arr: [],
-          char: 0,
-        },
-      ])
-      .map((line) => line.arr);
-  };
+      };
+      return words
+        .reduce(setWordLines, [
+          {
+            arr: [],
+            char: 0,
+          },
+        ])
+        .map((line) => line.arr);
+    },
+    []
+  );
   const style = {
     display: remaining > 0 ? "block" : "none",
   };
